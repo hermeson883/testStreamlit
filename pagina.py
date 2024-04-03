c672a4b14d76b56930ce9e3f1ab3b490892f548b
hermeson883 <hermesondovalebeserra722@gmail.com>
2024-04-02 23:58:55 -0300

    finalizando 2

--- pagina.py
+++ pagina.py
@@ -7,7 +7,7 @@ col1.map()
 
 col2.image("https://images.unsplash.com/photo-1581351123004-757df051db8e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGdhbWVzfGVufDB8fDB8fHww")
 
-col3.video("https://www.youtube.com/watch?v=s3sCmdBpusQ&t=185s&ab_channel=ANCAPSU")
+col3.video("https://www.youtube.com/watch?v=cK_jsTT4IL0&list=RDcK_jsTT4IL0&start_radio=1&ab_channel=AlfonsoRCA")
 
 
 st.map()
